84936434b1185986b114805fa7e319c25a11a81e
mathrcnvrsn <math.rcnvrsn@gmail.com>
2025-10-20 16:23:42 +0000

    simple hello world function

--- netlify/functions/search.js
+++ netlify/functions/search.js
@@ -1,37 +1,45 @@
 import fetch from 'node-fetch';
 import cheerio from 'cheerio';
 
-export async function handler(event, context) {
-  const url = "https://mediathequemallemort.opac-x.com/recherche?general="
-  const { query } = event.queryStringParameters || {};
-  if (!url || !query) {
+exports.handler = async event => {
+    const subject = event.queryStringParameters.name || 'World'
     return {
-      statusCode: 400,
-      body: JSON.stringify({ error: 'Les paramètres "url" et "query" sont requis.' }),
-    };
-  }
-  url += query;
-  
-  try {
-    const response = await fetch(url, { headers: { 'User-Agent': 'netlify-serverless' } });
-    if (!response.ok) {
-      return {
-        statusCode: response.status,
-        body: JSON.stringify({ error: `Erreur lors de la récupération de la page : ${response.statusText}` }),
-      };
+        statusCode: 200,
+        body: `Hello ${subject}!`,
     }
-    const html = await response.text();
-    const $ = cheerio.load(html);
-    const textContent = $('body').text();
-    const found = textContent.toLowerCase().includes(query.toLowerCase());
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ url, query, found }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.toString() }),
-    };
-  }
 }
+
+// export async function handler(event, context) {
+//   const url = "https://mediathequemallemort.opac-x.com/recherche?general="
+//   const { query } = event.queryStringParameters || {};
+//   if (!url || !query) {
+//     return {
+//       statusCode: 400,
+//       body: JSON.stringify({ error: 'Les paramètres "url" et "query" sont requis.' }),
+//     };
+//   }
+//   url += query;
+
+//   try {
+//     const response = await fetch(url, { headers: { 'User-Agent': 'netlify-serverless' } });
+//     if (!response.ok) {
+//       return {
+//         statusCode: response.status,
+//         body: JSON.stringify({ error: `Erreur lors de la récupération de la page : ${response.statusText}` }),
+//       };
+//     }
+//     const html = await response.text();
+//     const $ = cheerio.load(html);
+//     const textContent = $('body').text();
+//     const found = textContent.toLowerCase().includes(query.toLowerCase());
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ url, query, found }),
+//     };
+//   } catch (error) {
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: error.toString() }),
+//     };
+//   }
+// }
