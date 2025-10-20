212915592e5dde16a8f7b86ea172400e2f4a42cc
mathrcnvrsn <math.rcnvrsn@gmail.com>
2025-10-20 17:30:14 +0000

    KISSSS

--- netlify/functions/search.js
+++ netlify/functions/search.js
@@ -1,44 +1,36 @@
 import axios from 'axios';
+//import JSDOM from 'jsdom';
 
- export async function handler(event, context) {
-    const subject = event.queryStringParameters.name || 'World'
+export async function handler(event, context) {
+  const { query } = event.queryStringParameters || {};
+  if (!query) {
     return {
-        statusCode: 200,
-        body: `Hello ${subject}!`,
-    }
+      statusCode: 400,
+      body: JSON.stringify({ error: 'Les paramètres "url" et "query" sont requis.' }),
+    };
+  }
+
+    try {
+               
+    //     const response = await axios.get(`https://mediathequemallemort.opac-x.com/recherche?general=${query}`); //, { headers: { 'User-Agent': 'netlify-serverless' } });
+    
+    // if(response.status !== 200) {
+	// 		return {statusCode: response.status, body: `Localities fetch error: ${response.statusText}`};
+	// 	}
+
+    //     //const dom = new JSDOM(response.data);
+    //     //const found = dom.window.document.querySelector("div.titre_doc > a")
+         const found = true;
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify({ query, found }),
+
+		};
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.toString() }),
+    };
+  }
 }
-
-// export async function handler(event, context) {
-//   const url = "https://mediathequemallemort.opac-x.com/recherche?general="
-//   const { query } = event.queryStringParameters || {};
-//   if (!url || !query) {
-//     return {
-//       statusCode: 400,
-//       body: JSON.stringify({ error: 'Les paramètres "url" et "query" sont requis.' }),
-//     };
-//   }
-//   url += query;
-
-//   try {
-//     const response = await axios.get(url, { headers: { 'User-Agent': 'netlify-serverless' } });
-//     if (!response.ok) {
-//       return {
-//         statusCode: response.status,
-//         body: JSON.stringify({ error: `Erreur lors de la récupération de la page : ${response.statusText}` }),
-//       };
-//     }
-//     const html = await response.text();
-//     const $ = cheerio.load(html);
-//     const textContent = $('body').text();
-//     const found = textContent.toLowerCase().includes(query.toLowerCase());
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify({ url, query, found }),
-//     };
-//   } catch (error) {
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ error: error.toString() }),
-//     };
-//   }
-// }
