04f3de9c72073f6527e0a115f3571ec1ae1cde78
mathrcnvrsn <math.rcnvrsn@gmail.com>
2025-10-20 17:23:08 +0000

    KISS

--- netlify/functions/search.js
+++ netlify/functions/search.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import JSDOM from 'jsdom';
+//import JSDOM from 'jsdom';
 
 export async function handler(event, context) {
   const { query } = event.queryStringParameters || {};
@@ -11,16 +11,16 @@ export async function handler(event, context) {
   }
 
     try {
-    
-           
+               
         const response = await axios.get(`https://mediathequemallemort.opac-x.com/recherche?general=${query}`); //, { headers: { 'User-Agent': 'netlify-serverless' } });
     
-    if(response.status == 200) {
+    if(response.status !== 200) {
 			return {statusCode: response.status, body: `Localities fetch error: ${response.statusText}`};
 		}
 
-        const dom = new JSDOM(response.data);
-        const found = dom.window.document.querySelector("div.titre_doc > a")
+        //const dom = new JSDOM(response.data);
+        //const found = dom.window.document.querySelector("div.titre_doc > a")
+        const found = true;
 
 		return {
 			statusCode: 200,
