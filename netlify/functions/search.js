4ba255701ea528e1116b749ed0d7ee0e2848be9d
mathrcnvrsn <math.rcnvrsn@gmail.com>
2025-10-20 16:52:55 +0000

    search first step

--- netlify/functions/search.js
+++ netlify/functions/search.js
@@ -1,44 +1,35 @@
 import axios from 'axios';
 
- export async function handler(event, context) {
-    const subject = event.queryStringParameters.name || 'World'
+export async function handler(event, context) {
+  https://mediathequemallemort.opac-x.com/recherche?general="
+  const { query } = event.queryStringParameters || {};
+  if (!query) {
     return {
-        statusCode: 200,
-        body: `Hello ${subject}!`,
+      statusCode: 400,
+      body: JSON.stringify({ error: 'Les paramètres "url" et "query" sont requis.' }),
+    };
+  }
+
+  try {
+    const response = await axios.get(`https://mediathequemallemort.opac-x.com/recherche?general=${query}`); //, { headers: { 'User-Agent': 'netlify-serverless' } });
+    if (!response.ok) {
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({ error: `Erreur lors de la récupération de la page : ${response.statusText}` }),
+      };
     }
+    const html = await response.text();
+    // const $ = cheerio.load(html);
+    // const textContent = $('body').text();
+    // const found = textContent.toLowerCase().includes(query.toLowerCase());
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ url, query, "TODO" }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.toString() }),
+    };
+  }
 }
-
-// export async function handler(event, context) {
-//   const url = "https://mediathequemallemort.opac-x.com/recherche?general="
-//   const { query } = event.queryStringParameters || {};
-//   if (!url || !query) {
-//     return {
-//       statusCode: 400,
-//       body: JSON.stringify({ error: 'Les paramètres "url" et "query" sont requis.' }),
-//     };
-//   }
-//   url += query;
-
-//   try {
-//     const response = await axios.get(url, { headers: { 'User-Agent': 'netlify-serverless' } });
-//     if (!response.ok) {
-//       return {
-//         statusCode: response.status,
-//         body: JSON.stringify({ error: `Erreur lors de la récupération de la page : ${response.statusText}` }),
-//       };
-//     }
-//     const html = await response.text();
-//     const $ = cheerio.load(html);
-//     const textContent = $('body').text();
-//     const found = textContent.toLowerCase().includes(query.toLowerCase());
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify({ url, query, found }),
-//     };
-//   } catch (error) {
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ error: error.toString() }),
-//     };
-//   }
-// }
